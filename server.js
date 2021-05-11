09bf4987554b11a7ec8482a9dc1097d21fe86088
Ubuntu <ubuntu@ip-172-31-18-199.eu-central-1.compute.internal>
2021-05-11 17:18:24 +0000

    Structura backend

---
+++ server.js
@@ -0,0 +1,107 @@
+const express = require("express")
+
+const app = express()
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('my_db', 'theodor', 'password', {
+    dialect: "mysql",
+    host: "localhost"
+})
+
+sequelize.authenticate().then(() => {
+    console.log("Connected to database")
+}).catch((err) => {
+    console.log(err)
+    console.log("Unable to connect to database")
+})
+
+const Messages = sequelize.define('messages', {
+    subject: Sequelize.STRING,
+    name: Sequelize.STRING,
+    message: Sequelize.TEXT
+})
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+
+app.get('/createdb', (request, response) => {
+    sequelize.sync({force:true}).then(() => {
+        response.status(200).send('tables created')
+    }).catch((err) => {
+        console.log(err)
+        response.status(200).send('could not create tables')
+    })
+})
+
+app.get('/hello', (req, res) => {
+    res.status(200).send({"message" : "Hello world"})
+})
+
+app.get('/messages', (request, response) => {
+    Messages.findAll().then((results) => {
+        response.status(200).json(results)
+    })
+})
+
+
+app.get('/messages/:id', (request, response) => {
+    Messages.findByPk(request.params.id).then((result) => {
+        if(result) {
+            response.status(200).json(result)
+        } else {
+            response.status(404).send('resource not found')
+        }
+    }).catch((err) => {
+        console.log(err)
+        response.status(500).send('database error')
+    })
+})
+
+
+app.post('/messages', (req, res) => {
+    Messages.create(req.body).then((result) => {
+        res.status(201).json(result)
+    }).catch((err) => {
+        res.status(500).send("resource not created")
+    })
+})
+
+app.put('/messages/:id', (request, response) => {
+    Messages.findByPk(request.params.id).then((message) => {
+        if(message) {
+            message.update(request.body).then((result) => {
+                response.status(201).json(result)
+            }).catch((err) => {
+                console.log(err)
+                response.status(500).send('database error')
+            })
+        } else {
+            response.status(404).send('resource not found')
+        }
+    }).catch((err) => {
+        console.log(err)
+        response.status(500).send('database error')
+    })
+})
+
+app.delete('/messages/:id', (request, response) => {
+    Messages.findByPk(request.params.id).then((message) => {
+        if(message) {
+            message.destroy().then((result) => {
+                response.status(204).send()
+            }).catch((err) => {
+                console.log(err)
+                response.status(500).send('database error')
+            })
+        } else {
+            response.status(404).send('resource not found')
+        }
+    }).catch((err) => {
+        console.log(err)
+        response.status(500).send('database error')
+    })
+})
+ 
+app.listen(8080)
